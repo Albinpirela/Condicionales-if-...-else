211ed8f3b9452604c7399ec4d95da7efeacdc178
Albinpirela <119135868+Albinpirela@users.noreply.github.com>
2023-03-02 10:38:34 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let seleccionar = document.querySelector('select');
+console.log(seleccionar);
+let parrafo = document.querySelector('p');
+
+seleccionar.addEventListener('change', establecerClima);
+// seleccionar.onchange = establecerClima;
+
+function establecerClima() {
+  let eleccion = seleccionar.value;
+  if (eleccion === 'soleado') {
+    parrafo.textContent = 'El día esta agradable y soleado hoy. ¡Use pantalones cortos! Ve a la playa o al parque y come un helado.';
+  } else if (eleccion === 'lluvioso') {
+    parrafo.textContent = 'Está lloviendo, tome un abrigo para lluvia y un paraguas, y no se quede por fuera mucho tiempo.';
+  } else if (eleccion === 'nevando') {
+    parrafo.textContent = 'Está nevando ─ ¡está congelando! Lo mejor es quedarse en casa con una taza caliente de chocolate, o hacer un muñeco de nieve.';
+  } else if (eleccion === 'nublado') {
+    parrafo.textContent = 'No está lloviendo, pero el cielo está gris y nublado; podría llover en cualquier momento, así que lleve un saco solo por si acaso.';
+  } else {
+    parrafo.textContent = '';
+  }
+}
